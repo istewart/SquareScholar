2e10645b83d7eddaa154b6011101bcda2412de44
lrzhou1010 <lrzhou1010@gmail.com>
2017-02-04 16:53:21 -0500

    made search bar display

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello(name=None):
-    return render_template('index.html')
+    return render_template('index.html', article=[{'title': 'hello', 'text': 'world'}])

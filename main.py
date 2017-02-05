61d085cfdb3858a59f48071d5ea414c7d1e97023
istewart <istewart3141@gmail.com>
2017-02-04 20:24:12 -0500

    Started line chart.

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 from article_lister2 import get_articles
+from histograms import get_histos
 
 @app.route('/')
-def home(articles=None):
+def home():
     return render_template('index.html', articles=get_articles('hippocampus'))
+
+@app.route('/topics')
+def topics():
+    return get_histos('hippocampus')
